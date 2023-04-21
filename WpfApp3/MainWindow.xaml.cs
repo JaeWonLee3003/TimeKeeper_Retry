dac67a2f7a5a1dc8ca9e51985f1645d9e01a0ed4
JaeWonLee3003 <dlwodnjs3003@gmail.com>
2023-04-21 14:09:05 +0900

    now

--- WpfApp3/MainWindow.xaml.cs
+++ WpfApp3/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Collections.Generic;
 
 
 namespace WpfApp3
@@ -26,7 +29,30 @@ namespace WpfApp3
             setStartMin = DateTime.Now.Minute;
             setStartSec = DateTime.Now.Second;
         }
+        private void CreateDashboard()
+        {
+            // 그리드 생성
+            
+            var dashboard = new Grid();
 
+            // 그리드에 텍스트블럭 추가
+            var textBlock = new TextBlock();
+            textBlock.Text = "알림 대시보드";
+            textBlock.FontSize = 20;
+            textBlock.Margin = new Thickness(20, 20, 0, 0);
+            textBlock.VerticalAlignment = VerticalAlignment.Top;
+            textBlock.HorizontalAlignment = HorizontalAlignment.Left;
+            dashboard.Children.Add(textBlock);
+
+            // 그리드에 리스트박스 추가
+            var listBox = new ListBox();
+            listBox.Margin = new Thickness(20, 60, 20, 20);
+            listBox.ItemsSource = notificationList; // notificationList는 알림 목록 리스트입니다.
+            dashboard.Children.Add(listBox);
+
+            // 창에 그리드 추가
+            Content = dashboard;
+        }
 
         /// <summary>
         /// 메인 버튼 중 "앱 가이드" 창으로 넘어가는 버튼
@@ -46,8 +72,9 @@ namespace WpfApp3
         /// <param name="e"></param>
         private void Main_Btn2_Click(object sender, RoutedEventArgs e)
         {
-            WpfApp3.Second_Content Main_Btn2_Click = new WpfApp3.Second_Content();
-            Main_Btn2_Click.ShowDialog(); // 창을 닫기전에 이전 앱을 닫지 못함.
+            CreateDashboard();
+            //WpfApp3.Second_Content Main_Btn2_Click = new WpfApp3.Second_Content();
+            //Main_Btn2_Click.ShowDialog(); // 창을 닫기전에 이전 앱을 닫지 못함.
         }
 
         /// <summary>
