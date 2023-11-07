a2077978a5568834045b86ab3765ecede7c48f72
재원 이 <kenneth.lee@latisglobal.net>
2023-11-07 17:02:07 +0900

    다른 클래스 메서드 호출 에러 고치기

--- WpfApp3/MainWindow.xaml.cs
+++ WpfApp3/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows.Controls;
 using System.Windows.Media;
 using System.Collections.Generic;
 using System.Windows.Media.Imaging;
+using WpfApp3.SideDisplay;
 
 namespace WpfApp3
 {
@@ -12,60 +13,22 @@ namespace WpfApp3
     /// </summary>
     public partial class MainWindow : Window
     {
-
-        private DateTime startTime;
-        private Grid dashboard; // dashboard 필드 추가
-
         public MainWindow()
         {
             InitializeComponent();
-
-            this.Loaded += MainWindow_Loaded;
-
-            startTime = DateTime.Now;
-            Main_Btn1.Click += Main_Btn1_Click;
-            Main_Btn2.Click += Main_Btn2_Click;
-            Main_Btn3.Click += Main_Btn3_Click;
+            /*this.Loaded += MainWindow_Loaded;*/
 
         }
 
-        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        public void HandleAlarmButtonClick()
         {
-
-
+            AddAlramPopup();
         }
-
-
-
-
-        /// <summary>
-        /// 메인 버튼 중 "앱 가이드" 창으로 넘어가는 버튼
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void Main_Btn1_Click(object sender, RoutedEventArgs e)
+        public void AddAlramPopup()
         {
-            /*Body.Child = dashboard;*/
-        }
-
-        /// <summary>
-        /// 메인 버튼 중 "알림 대시보드" 창으로 넘어가는 버튼
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void Main_Btn2_Click(object sender, RoutedEventArgs e)
-        {
-            /*Body.Child = dashboard;*/
-        }
-
-        /// <summary>
-        /// 메인 버튼 중 "PC 사용 분석" 창으로 넘어가는 버튼
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void Main_Btn3_Click(object sender, RoutedEventArgs e)
-        {
-            /*Body.Child = dashboard;*/
+            AddAlramPopup addAlramPopup = new AddAlramPopup();
+            EffectPanel.Children.Clear();
+            EffectPanel.Children.Add(addAlramPopup);
         }
     }
 }
